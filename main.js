cdc3b4bf45c1533fed5b4cfdc5536cf5edd7cfa8
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-03-07 13:06:44 +0000

    Pagination

--- main.js
+++ main.js
@@ -10,30 +10,64 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
+const scrollElement = document.getElementById('main');
+
+const prev = document.getElementById('previous');
+const next = document.getElementById('next');
+const current = document.getElementById('current');
+
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+
+
 getMovies(API_URL);
 
 const moodButtons = document.querySelectorAll('.mood-btn');
 moodButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const scrollElement = document.getElementById('main');
         const selectedMoodGenre = button.getAttribute('data-genre');
         const selectedMoodKeyword = button.getAttribute('data-keyword');
         const selectedMoodGenreExcluded = button.getAttribute('data-genre-exclude');
         const moodURL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&with_genres=${selectedMoodGenre}&with_keywords=${selectedMoodKeyword}&without_genres=${selectedMoodGenreExcluded}&api_key=${API_KEY}`;
         getMovies(moodURL);
         scrollElement.scrollIntoView({behavior: 'smooth'});
-        console.log(moodURL);
     });
 });
 
 // Data from TMDB
 function getMovies(url) {
+    lastUrl = url;
     fetch(url)
         .then(res => res.json())
         .then(data => {
             if (data.results) {
                 showMovies(data.results);
                 console.log(data.results);
+                currentPage = data.page;
+                nextPage = currentPage + 1;
+                prevPage = currentPage -1;
+                totalPages = data.total_pages;
+
+                current.innerText = currentPage;
+
+                if(currentPage <=1) {
+                    prev.classList.add('disabled');
+                    next.classList.remove('disabled');
+                }else if(currentPage>=totalPages) {
+                    prev.classList.remove('disabled');
+                    next.classList.add('disabled');
+                }else {
+                    prev.classList.remove('disabled');
+                    next.classList.remove('disabled');
+                }
+
+                scrollElement.scrollIntoView({behavior: 'smooth'});
+
             } else {
                 console.error("No results found");
             }
@@ -79,7 +113,7 @@ function getColor(vote) {
     }
 }
 
-// Search for name
+/*Search for name
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -90,4 +124,37 @@ form.addEventListener('submit', (e) => {
     } else {
         getMovies(API_URL);
     }
-});
+});*/
+
+// Pagination functions
+
+prev.addEventListener('click', () => {
+    if(prevPage > 0){
+        pageCall(prevPage);
+    }
+})
+
+next.addEventListener('click', () => {
+    if(nextPage <= totalPages){
+        pageCall(nextPage);
+        
+    }
+})
+
+function pageCall(page){
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length -1].split('=');
+    if(key[0] != 'page') {
+        let url = lastUrl + '&page='+page;
+        getMovies(url);
+    }else {
+        key[1] = page.toString();
+        let joinA = key.join('=');
+        queryParams[queryParams.length -1] = joinA;
+        let joinB = queryParams.join('&');
+        let url = urlSplit[0] +'?'+joinB;
+        getMovies(url);
+    }
+
+}
